3f82bb8d08f95f6facd2141e6c929c87ba9a216b
TejasAppireddy <tejas.appireddy@gmail.com>
2026-07-22 11:45:58 -0400

    test

---
+++ quantum_computing_fundamentals_1/loops.py
@@ -0,0 +1,6 @@
+name = "tejas"
+for i in range(1):
+    print(name)
+
+for index in range(1,11):
+    print("Room ", index, " Checked.")
---
+++ quantum_computing_fundamentals_1/my_first_python_code.py
@@ -0,0 +1,20 @@
+# Create 3 variables. An int, a string and a boolean
+
+my_int = 5
+name = "tejas"
+is_python_fun = True
+
+
+
+# Create an if statement. If your int variable is greater than 3, Display the message "Yes it's greater than 3" else display, "It's not greater than 3"
+
+if my_int > 3:
+    print("Yes it is greater than 3")
+else:
+     print("No it is not greather than 3")
+
+
+# Create code that asks a user for their name and then display what the user typed
+
+name = input("What is your name")
+print(name)
